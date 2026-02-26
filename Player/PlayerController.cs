2a6d75372c77cc69c99eaa5ad12ee0480b52df72
oghatmake1 <wmwwwb@gmail.com>
2026-02-26 14:03:33 -0500

    Indent

--- Player/PlayerController.cs
+++ Player/PlayerController.cs
@@ -2,107 +2,106 @@ using System;
 using Godot;
 
 [SceneGlobal]
-public partial class PlayerController : CharacterControllerBase {
-	[Export] public required AudioStream Step;
-	[Export] public required AnimationPlayer Animations;
-	[Export] public required Node3D CameraRail;
-	[Export] public required Camera3D Camera;
-	[Export] public float ForwardSpeed = 1.2f;
-	[Export] public float BackwardSpeed = 0.9f;
-	[Export] public float SidewardsSpeed = 0.48f;
-	[Export] public byte Brightness = 40;
-	[Export] public float FogNear = 1f;
-	[Export] public float FogFar = 3f;
-	[Export] float MouseSensitivity = 0.2f;
-	[Export] float ControllerSensitivity = 300f;
-	[Export] 
-	
-	public float DeathBrightnessLerp {
-		get;
-		set {
-			field = value;
-			if (value == 0f) {
-				Helpers.SetBrightness(Brightness);
-				return;
-			}
-			var subtract = float.Lerp(0f, 90f / 255f, value);
-			Helpers.SetBrightness(Color.Color8(255, 100, 100) - new Color(subtract, subtract, subtract));
-		}
-	}
+public partial class PlayerController: CharacterControllerBase {
+    [Export] public required AudioStream Step;
+    [Export] public required AnimationPlayer Animations;
+    [Export] public required Node3D CameraRail;
+    [Export] public required Camera3D Camera;
+    [Export] public float ForwardSpeed = 1.2 f;
+    [Export] public float BackwardSpeed = 0.9 f;
+    [Export] public float SidewardsSpeed = 0.48 f;
+    [Export] public byte Brightness = 40;
+    [Export] public float FogNear = 1 f;
+    [Export] public float FogFar = 3 f;
+    [Export] float MouseSensitivity = 0.2 f;
+    [Export] float ControllerSensitivity = 300 f;
+    [Export]
 
-	private bool _isDead;
-	private static readonly Vector3 HeadBobInterval = new Vector3(80f, 40f, 1f) / 60f;
-	private static readonly Vector3 HeadBobOffset = new(0.08f, 0.1f, 0f);
-	private float _headBobAcc;
+    public float DeathBrightnessLerp {
+      get;
+      set {
+        field = value;
+        if (value == 0 f) {
+          Helpers.SetBrightness(Brightness);
+          return;
+        }
+        var subtract = float.Lerp(0 f, 90 f / 255 f, value);
+        Helpers.SetBrightness(Color.Color8(255, 100, 100) - new Color(subtract, subtract, subtract));
+      }
+    }
 
-	public int Floor => Helpers.GetFloor(GlobalPosition - new Vector3(0f, 0.5f, 0f));
+    private bool _isDead;
+    private static readonly Vector3 HeadBobInterval = new Vector3(80 f, 40 f, 1 f) / 60 f;
+    private static readonly Vector3 HeadBobOffset = new(0.08 f, 0.1 f, 0 f);
+    private float _headBobAcc;
 
-	public override void _Ready() {
-		Input.MouseMode = Input.MouseModeEnum.Captured;
-		SetFogRange(FogNear, FogFar);
-		Helpers.SetBrightness(Brightness);
+    public int Floor => Helpers.GetFloor(GlobalPosition - new Vector3(0 f, 0.5 f, 0 f));
 
-		var r = CameraRail.Rotation;
-	}
+    public override void _Ready() {
+      Input.MouseMode = Input.MouseModeEnum.Captured;
+      SetFogRange(FogNear, FogFar);
+      Helpers.SetBrightness(Brightness);
 
-	public override void _Input(InputEvent @event) {
-		if (@event is InputEventMouseMotion mm) {
-			var invertedY = -mm.Relative.Y;
-			Vector2 delta = new Vector2(mm.Relative.X, -invertedY) * MouseSensitivity;
-			HandleLook(delta);
-		}
-	}
+      var r = CameraRail.Rotation;
+    }
 
-	public override void _Process(double delta) {
-		float lookRight = Input.GetActionStrength("LookRight") - Input.GetActionStrength("LookLeft");
-		float lookUp = Input.GetActionStrength("LookDown") - Input.GetActionStrength("LookUp");
+    public override void _Input(InputEvent @event) {
+      if (@event is InputEventMouseMotion mm) {
+        var invertedY = -mm.Relative.Y;
+        Vector2 delta = new Vector2(mm.Relative.X, -invertedY) * MouseSensitivity;
+        HandleLook(delta);
+      }
+    }
 
-		if (lookRight != 0f || lookUp != 0f) {
-			Vector2 lookDelta = new Vector2(lookRight * ControllerSensitivity, lookUp * ControllerSensitivity);
-			HandleLook(lookDelta * (float)delta);
-		}
-	}
-	
-private void HandleLook(Vector2 lookDelta) {
-	var r = CameraRail.RotationDegrees;
-	float minmaxpitch = 89.999f; // this being exactily 90 would mess up the movement when looking at that angle
-	float minPitch = -minmaxpitch;
-	float maxPitch = minmaxpitch;
+    public override void _Process(double delta) {
+      float lookRight = Input.GetActionStrength("LookRight") - Input.GetActionStrength("LookLeft");
+      float lookUp = Input.GetActionStrength("LookDown") - Input.GetActionStrength("LookUp");
 
-	// Apply lookDelta
-	r.X -= lookDelta.Y;
-	r.Y += -lookDelta.X;
+      if (lookRight != 0 f || lookUp != 0 f) {
+        Vector2 lookDelta = new Vector2(lookRight * ControllerSensitivity, lookUp * ControllerSensitivity);
+        HandleLook(lookDelta * (float) delta);
+      }
+    }
 
-	r.X = Mathf.Clamp(r.X, minPitch, maxPitch);
+    private void HandleLook(Vector2 lookDelta) {
+      var r = CameraRail.RotationDegrees;
+      float minmaxpitch = 89.999 f; // this being exactily 90 would mess up the movement when looking at that angle
+      float minPitch = -minmaxpitch;
+      float maxPitch = minmaxpitch;
 
-	// Apply rotation
-	CameraRail.RotationDegrees = new Vector3(r.X, r.Y, 0f);
-}
-	public override void _PhysicsProcess(double deltaD) {
-		var inputDir = _isDead ? Vector2.Zero : new(
-			SidewardsSpeed * (Input.GetActionStrength(Actions.MoveRight) - Input.GetActionStrength(Actions.MoveLeft)),
-			Input.GetActionStrength(Actions.MoveBackward) != 0 ^ Input.GetActionStrength(Actions.MoveForward) != 0
-				? -ForwardSpeed * (Input.GetActionStrength(Actions.MoveForward))
-				  + BackwardSpeed * (Input.GetActionStrength(Actions.MoveBackward))
-				: 0
-		);
+      // Apply lookDelta
+      r.X -= lookDelta.Y;
+      r.Y += -lookDelta.X;
 
-		var rightDir = (-Camera.GlobalBasis.Z).Cross(UpDirection).Normalized();
-		var forwardDir = rightDir.Cross(UpDirection).Normalized();
+      r.X = Mathf.Clamp(r.X, minPitch, maxPitch);
 
-		Velocity = (rightDir * inputDir.X + forwardDir * inputDir.Y) with {
-			Y = Velocity.Y,
-		};
+      // Apply rotation
+      CameraRail.RotationDegrees = new Vector3(r.X, r.Y, 0 f);
+    }
+    public override void _PhysicsProcess(double deltaD) {
+        var inputDir = _isDead ? Vector2.Zero : new(
+          SidewardsSpeed * (Input.GetActionStrength(Actions.MoveRight) - Input.GetActionStrength(Actions.MoveLeft)),
+          Input.GetActionStrength(Actions.MoveBackward) != 0 ^ Input.GetActionStrength(Actions.MoveForward) != 0 ?
+          -ForwardSpeed * (Input.GetActionStrength(Actions.MoveForward)) +
+          BackwardSpeed * (Input.GetActionStrength(Actions.MoveBackward)) :
+          0
+        );
 
-		if (inputDir != Vector2.Zero) {
-			var oldAcc = _headBobAcc;
-			_headBobAcc = (_headBobAcc + (float)deltaD) % MathF.Max(HeadBobInterval.X, HeadBobInterval.Y);
-			if (_headBobAcc % HeadBobInterval.Y < oldAcc % HeadBobInterval.Y) {
-				AudioManager.PlaySound(Step);
-			}
-			var intervalHalf = HeadBobInterval / 2f;
-			Camera.Position = HeadBobOffset * (new Vector3(1f, 1f, 1f)
-											   - (new Vector3(_headBobAcc, _headBobAcc, _headBobAcc) % HeadBobInterval
+        var rightDir = (-Camera.GlobalBasis.Z).Cross(UpDirection).Normalized();
+        var forwardDir = rightDir.Cross(UpDirection).Normalized();
+
+        Velocity = (rightDir * inputDir.X + forwardDir * inputDir.Y) with {
+          Y = Velocity.Y,
+        };
+
+        if (inputDir != Vector2.Zero) {
+          var oldAcc = _headBobAcc;
+          _headBobAcc = (_headBobAcc + (float) deltaD) % MathF.Max(HeadBobInterval.X, HeadBobInterval.Y);
+          if (_headBobAcc % HeadBobInterval.Y < oldAcc % HeadBobInterval.Y) {
+            AudioManager.PlaySound(Step);
+          }
+          var intervalHalf = HeadBobInterval / 2 f;
+          Camera.Position = HeadBobOffset * (new Vector3(1 f, 1 f, 1 f)											   - (new Vector3(_headBobAcc, _headBobAcc, _headBobAcc) % HeadBobInterval
 												  - intervalHalf).Abs() / intervalHalf);
 		}
 
